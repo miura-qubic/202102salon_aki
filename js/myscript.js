ce09888a8b96914736b6d512a48381bf8430ec06
miura <miura@qu-bic.jp>
2021-02-04 15:04:18 +0900

    トップ：作成（pc、tab）

--- js/myscript.js
+++ js/myscript.js
@@ -8,10 +8,14 @@ const tabletwidth = 1025;
 {
 	const openMenu = document.getElementById('menu_open');
 	const Nav = document.querySelector('header nav');
+	const body = document.querySelector('body');
+	const overlay = document.getElementById('overlay');
 
 	openMenu.addEventListener('click', function () {
 		openMenu.classList.toggle('active');
 		Nav.classList.toggle('active');
+		body.classList.toggle('active');
+		overlay.classList.toggle('active');
 	});
 }
 
@@ -23,6 +27,14 @@ $(function () {
 		if($(window).scrollTop() > 500){
 			$('.header_in').addClass('active');
 		}else{
+			$('.header_in').removeClass('active');
+		}
+	});
+	
+	$('header .menu_open').on('click',function(){
+		if($(this).hasClass('active')){
+			$('.header_in').addClass('active');
+		}else if(!$(this).hasClass('active') && $(window).scrollTop() <= 500){
 			$('.header_in').removeClass('active');
 		}
 	});
